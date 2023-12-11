4170ed343ec427fe8c640e8972e9352bcfa5896b
tartoura <bitarthierry@gmail.com>
2023-12-11 18:56:41 +0100

    First implementation of Convention Plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,54 +1,13 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    //alias(libs.plugins.android.application)
+    //alias(libs.plugins.kotlin.android)
     alias(libs.plugins.dagger.hilt.android)
     id(libs.plugins.kotlin.kapt.get().pluginId)
+    id("thierry.android.application")
 }
 
 android {
     namespace = "thierry.bitcoin"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "thierry.bitcoin"
-        minSdk = 24
-        targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.2"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
 }
 
 dependencies {
---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    `kotlin-dsl`
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+/*tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+}*/
+
+dependencies {
+
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
+}
+
+gradlePlugin {
+    plugins {
+        register("androidApplication") {
+            id = "thierry.android.application"
+            implementationClass = "AndroidApplicationConventionPlugin"
+        }
+    }
+}
